5974859b210c19c5bb7b5ed432e66bf80a54a9cc
neko-tenshi <32102096+MireilleVignal@users.noreply.github.com>
2022-02-15 15:38:40 +0100

    début de la death star

--- tinyraytracer.cpp
+++ tinyraytracer.cpp
@@ -234,6 +234,7 @@ int main() {
     Material     mirror(1.0, Vec4f(0.0, 10.0, 0.8, 0.0), Vec3f(1.0, 1.0, 1.0), 1425.);
 
     std::vector<Sphere> spheres;
+    spheres.push_back(Sphere(Vec3f(-3.5, 1.75, -14.75), 0.7, Material(0, Vec4f(0, 0, 0, 0), Vec3f(0, 0, 0), 0)));
     spheres.push_back(Sphere(Vec3f(-3,    0,   -16), 2,      ivory));
     spheres.push_back(Sphere(Vec3f(-1.0, -1.5, -12), 2,      glass));
     spheres.push_back(Sphere(Vec3f( 1.5, -0.5, -18), 3, red_rubber));
